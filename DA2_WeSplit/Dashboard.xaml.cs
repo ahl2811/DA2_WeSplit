edf62e13e4602aa53109b7ca752fcf334106c33f
Le Hao <o18ahl2811@gmail.com>
2020-12-05 21:00:22 +0700

    Initial commit

---
+++ DA2_WeSplit/Dashboard.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DA2_WeSplit.Screens;
+namespace DA2_WeSplit
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+            MainScreen.Children.Add(new TripScreen());
+        }
+
+
+        private void RowDefinition_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(this.WindowState != WindowState.Maximized)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void AllTripButton_Click(object sender, RoutedEventArgs e)
+        {
+     
+            MainScreen.Children.Clear();
+            MainScreen.Children.Add(new TripScreen());
+        }
+
+
+        private void CurrentTripButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainScreen.Children.Clear();
+            MainScreen.Children.Add(new TripScreen());
+        }
+
+        private void PassedTripButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainScreen.Children.Clear();
+            MainScreen.Children.Add(new TripScreen());
+        }
+
+        private void SettingButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainScreen.Children.Clear();
+            MainScreen.Children.Add(new SettingScreen());
+        }
+
+        private void SupportButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainScreen.Children.Clear();
+            MainScreen.Children.Add(new SupportScreen());
+        }
+    }
+
+}
---
+++ DA2_WeSplit/Screens/TripScreen.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DA2_WeSplit.Screens
+{
+    /// <summary>
+    /// Interaction logic for TripScreen.xaml
+    /// </summary>
+    public partial class TripScreen : UserControl
+    {
+        public TripScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            keywordPlaceholderTextBlock.Visibility = Visibility.Hidden;
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (keywordTextBox.Text.Length == 0)
+            {
+                keywordPlaceholderTextBlock.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
